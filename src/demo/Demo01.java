890afef6fd20ddfdac7d4ffc4448eb03428be028
wangzhuwei <wangzhuwei@industics.com>
2020-12-28 16:07:01 +0800

    第一次提交:aaa分支测试

--- src/demo/Demo01.java
+++ src/demo/Demo01.java
@@ -4,5 +4,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("wzw分支中的内容");
         System.out.println("Demo01");
+        System.out.println("aaa");
     }
 }
